f6f079bcd55ba8b5be907752fce2c92637f51e0b
SHIVA9964599018 <38631570+SHIVA9964599018@users.noreply.github.com>
2025-06-14 02:20:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -482,4 +482,8 @@ installBtn.addEventListener('click', () => {
   }
 });
 
-
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('service-worker.js')
+    .then(reg => console.log("✅ SW registered"))
+    .catch(err => console.error("❌ SW registration failed:", err));
+}
