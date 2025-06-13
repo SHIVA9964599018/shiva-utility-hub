1229305b114df0a056a9f944f67a7ec7bd77d084
SHIVA9964599018 <38631570+SHIVA9964599018@users.noreply.github.com>
2025-06-13 13:55:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1178,12 +1178,10 @@ window.addEventListener('beforeinstallprompt', (e) => {
 window.showUtilitySubSection = function (subSectionId) {
   console.log(`Switching to Utility Sub-section: ${subSectionId}`);
 
-  // Hide all utility sub-sections
   document.querySelectorAll("div[id^='utility-']").forEach((el) => {
     el.style.display = "none";
   });
 
-  // Show the selected sub-section
   const subSection = document.getElementById(subSectionId);
   if (subSection) {
     subSection.style.display = "block";
@@ -1191,4 +1189,5 @@ window.showUtilitySubSection = function (subSectionId) {
     console.error(`Utility sub-section '${subSectionId}' not found.`);
   }
 };
+
 
